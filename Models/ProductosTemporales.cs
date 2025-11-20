a8370d5502a7638bc6e7e92348a66a0b9d8c34a6
UrielHamu <49518164@est.ort.edu.ar>
2025-11-20 15:55:42 -0300

    Add constructor to ProductosTemporales class

--- Models/ProductosTemporales.cs
+++ Models/ProductosTemporales.cs
@@ -10,4 +10,16 @@ public class ProductosTemporales
     public int IdLocal;
     public int PrecioConDescuento;
     public int IdLocalesProductosInicial;
+
+    public ProductosTemporales(Productos producto, LocalesProductosInicial localesProductosInicial, LocalesProductosVto localesProductosVto){
+        Nombre=producto.Nombre;
+        Categoria= BD.TraerCategoria(producto.IdCategoria);
+        FechaVencimiento=localesProductosVto.FechaVencimiento;
+        Cantidad=LocalesProductosVto.Cantidad;
+        Foto=producto.Foto;
+        PrecioInicial=localesProductosInicial.PrecioInicial;
+        IdLocal=localesProductosInicial.IdLocal;
+        PrecioConDescuento=localesProductosVto.PrecioConDescuento;
+        IdLocalesProductosInicial=localesProductosInicial.Id;
+    }
 }
